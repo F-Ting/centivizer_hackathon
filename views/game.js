4dec4a46008cc2a03f9c17dd6b7f9500b1b9f524
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-06-16 11:16:12 -0400

    Add sound effect for sprite movement

--- views/game.js
+++ views/game.js
@@ -1,3 +1,10 @@
+var audioContext;
+try {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+} catch (e) {
+    console.error(e);
+}
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -12,7 +19,10 @@ var config = {
         preload: preload,
         create: create,
         update:update
-    }
+    },
+    audio: {
+       context: audioContext
+   }
 };
 var game = new Phaser.Game(config);
 var circle;
@@ -22,6 +32,7 @@ var direction = true;
 var movement = true;
 var correct = true;
 var answer = "triangle";
+var ding;
 
 function preload ()
 {
@@ -30,7 +41,7 @@ function preload ()
     this.load.image('square', './assets/square.png');
     this.load.image('circle', './assets/circle.png');
     this.load.image('triangle', 'assets/triangle.png');
-
+    this.load.audio('ding', ['./assets/ding.mp3']);
 }
 
 function create ()
@@ -41,6 +52,7 @@ function create ()
     triangle = this.physics.add.sprite(400, 100, 'triangle');
     square = this.physics.add.sprite(700, 100, 'square');
 
+    ding = this.sound.add('ding');
 }
 
 function update ()
@@ -61,6 +73,7 @@ function update ()
             if (sprite.y>=70){
                 sprite.y-=3;
             }else{
+                ding.play();
                 direction =!direction;
             }
         }else{
